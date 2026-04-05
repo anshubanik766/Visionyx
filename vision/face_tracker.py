546eb72a44987433abcbf093ecdf9a20cad0c696
Anshu Banik <158592119+anshubanik766@users.noreply.github.com>
2026-04-05 16:51:41 +0530

    Update face_tracker.py

--- vision/face_tracker.py
+++ vision/face_tracker.py
@@ -1,25 +1,77 @@
 import cv2
-from deepface import DeepFace
+import mediapipe as mp
 
 
 class FaceTracker:
-    def __init__(self):
-        self.age = None
-        self.frame_count = 0
+    def __init__(self, min_detection_confidence=0.5):
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
+            static_image_mode=False,
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=min_detection_confidence,
+            min_tracking_confidence=0.5
+        )
 
-    def get_age(self, frame):
-        self.frame_count += 1
+        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_styles = mp.solutions.drawing_styles
 
-        # Run every 30 frames (performance optimization)
-        if self.frame_count % 30 == 0:
-            try:
-                analysis = DeepFace.analyze(
-                    frame,
-                    actions=['age'],
-                    enforce_detection=False
-                )
-                self.age = analysis[0]['age']
-            except:
-                pass
+    def process(self, frame, draw=True):
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.face_mesh.process(rgb)
 
-        return self.age
+        faces = []
+
+        if results.multi_face_landmarks:
+            h, w, _ = frame.shape
+
+            for face_landmarks in results.multi_face_landmarks:
+
+                if draw:
+                    self.mp_draw.draw_landmarks(
+                        image=frame,
+                        landmark_list=face_landmarks,
+                        connections=self.mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=self.mp_styles.get_default_face_mesh_tesselation_style()
+                    )
+
+                    # eyes outline
+                    self.mp_draw.draw_landmarks(
+                        image=frame,
+                        landmark_list=face_landmarks,
+                        connections=self.mp_face_mesh.FACEMESH_LEFT_EYE,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=self.mp_styles.get_default_face_mesh_contours_style()
+                    )
+
+                    self.mp_draw.draw_landmarks(
+                        image=frame,
+                        landmark_list=face_landmarks,
+                        connections=self.mp_face_mesh.FACEMESH_RIGHT_EYE,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=self.mp_styles.get_default_face_mesh_contours_style()
+                    )
+
+                x_min, y_min = w, h
+                x_max, y_max = 0, 0
+
+                for lm in face_landmarks.landmark:
+                    x, y = int(lm.x * w), int(lm.y * h)
+
+                    if x < x_min:
+                        x_min = x
+                    if y < y_min:
+                        y_min = y
+                    if x > x_max:
+                        x_max = x
+                    if y > y_max:
+                        y_max = y
+
+                bbox = (x_min, y_min, x_max - x_min, y_max - y_min)
+
+                faces.append({
+                    "bbox": bbox
+                })
+
+        return faces
